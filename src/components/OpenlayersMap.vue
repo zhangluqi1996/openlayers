6b44659150375eefab6efe29de91998ae965927e
zhangluqi <luqizh@126.com>
2021-01-06 22:18:35 +0800

    基本的地图显示

---
+++ src/components/OpenlayersMap.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="map" style="width:1200px;height:600px;"></div>
+</template>
+
+<script>
+  import 'ol/ol.css'
+  import {Map,View} from 'ol'
+  import TileLayer from 'ol/layer/Tile'
+  import OSM from 'ol/source/OSM'
+
+  export default {
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    //---地图初始化---
+    initMap() {
+      new Map({
+        target: "map",
+        layers: [
+          new TileLayer({
+            source: new OSM()
+          })
+        ],
+        view: new View({
+          projection: "EPSG:4326",
+          center: [118.51064429904429, 37.42444378968362],
+          zoom: 5
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #map {
+    height: 100%;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <OpenlayersMap />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import OpenlayersMap from "@/components/OpenlayersMap";
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: {OpenlayersMap}
 }
 </script>
+
+
+<style scoped>
+  .home {
+    height: 100%;
+  }
+</style>
